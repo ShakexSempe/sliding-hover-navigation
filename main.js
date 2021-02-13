05404121aed1797a1c28a35c251b24e0bf5a3730
Ray-Shakex <bshakex@gmail.com>
2021-02-13 13:12:32 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log("clicked:", mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'style.css'
+    }
+    if(mode == 'dark'){
+        document.getElementById('theme-style').href = 'dark.css'
+    }
+    
+    localStorage.setItem('theme', mode)
+}
